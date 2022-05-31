1831b6c6f6e8b1f232b51170b195d871d2860d67
ferytell <darkstar2709@gmail.com>
2022-05-31 23:10:16 +0700

    adding encription?

--- app/config/database-configurations.js
+++ app/config/database-configurations.js
@@ -1,5 +1,9 @@
+const CryptoJS = require ("crypto-js");
+const SHA = CryptoJS.SHA1("message");
+const encrypted = SHA.toString(CryptoJS.enc.Base64);
+const decrypted = CryptoJS.AES.decrypt("U2FsdGVkX180kQLWx3fjFT4aF7CjtpRtxrHgzm3g13Y=", encrypted).toString(CryptoJS.enc.Utf8);
 
-a1 = ["localhost","root","Linda0306^_^", "bangkit"];  //using 3306
+a1 = ["localhost","root",decrypted, "bangkit"];  //using 3306
 a2 = ["localhost","root","explosion", "bangkit"]; //using 5000
 
 
