702d2c987afe6e1fd3ddba3083f028361b692896
ferytell <darkstar2709@gmail.com>
2022-06-01 10:59:15 +0700

    change mySql creteConnection() into createPool()

--- app/constructors/database-connection.js
+++ app/constructors/database-connection.js
@@ -1,8 +1,64 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/database-configurations");
+const util = require('util');
+
+// Create a connection to the database
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+});
+
+
+// open the MySQL Pool connection
+connection.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
+
+  if (connection) connection.release()
+  console.log("Database connected...");
+
+  return
+})
+
+// Promisify for Node.js async/await.
+connection.query = util.promisify(connection.query)
 
 
 
+
+module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --------------------------------------------------------------------
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: dbConfig.HOST,
@@ -20,3 +76,47 @@ connection.connect(error => {
 
 
 module.exports = connection;
+---------------------------------------------------------------------
+*/ 
+
+
+
+/*
+I want to change connectionm into pool, because pool have advantages (tidak bergantung pada stu koneksi sql aja)
+but we need lot's of test 
+
+const util = require('util')
+const mysql = require('mysql')
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'my_database'
+})
+
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
+
+  if (connection) connection.release()
+
+  return
+})
+
+// Promisify for Node.js async/await.
+pool.query = util.promisify(pool.query)
+
+module.exports = pool
+
+*/
